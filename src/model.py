3294132c1b22572f0eab0564fcbb074336c0f475
Andrej Royan <andrej.royan@epitech.eu>
2025-12-06 14:35:00 +0100

    ADD test placeholders

---
+++ src/model.py
@@ -0,0 +1,3 @@
+class UmbraModel:
+    def __init__(self):
+        pass
---
+++ tests/test_model.py
@@ -0,0 +1,5 @@
+from src.model import UmbraModel
+
+def test_model_init():
+    model = UmbraModel()
+    assert model is not None
